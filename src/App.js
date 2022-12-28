293d6d38ac76936a74d740bb590cad073ef57b92
Sarthak-Srivastava-71449 <113346239+Sarthak-Srivastava-71449@users.noreply.github.com>
2022-12-28 17:28:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import CarouselHooks from "./components/CarouselHooks";
+import "./App.css";
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CarouselHooks />
+      </div>
+    );
+  }
+}
